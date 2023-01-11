a213353a3d0601301b3befb0df99b36def500950
Anshuman202 <asanshuman2@gmail.com>
2023-01-11 22:15:49 +0530

    reads the file

---
+++ ReadData.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class ReadData
+    {
+        public InventoeryModel Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoeryModel>(json);
+
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+
+    }
+}
+    
+
