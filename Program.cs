1dff8a9b41776a5f63fc31c08dd9115edd5ec865
Anshuman202 <asanshuman2@gmail.com>
2023-01-12 22:26:02 +0530

    main program to print and calculate stocks

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ namespace StockManagement
 {
     class Program
     {
+
         public static void Main(string[] args)
         {
+            int z = 1;
+
+
             int total = 0, total1 = 0, total2 = 0;
             string path = "E:\\InventoryManagement\\InventoryManagement\\Stock.json";
             ReadData obj = new ReadData();
@@ -68,6 +72,62 @@ namespace StockManagement
             }
             Console.WriteLine("The  total value for Hybrid Stocks is : {0}", total2);
             Console.WriteLine();
+
+
+
+
+
+            Console.WriteLine("enter the filename to accout");
+            string fileName = Console.ReadLine();
+
+            stockAccount stock49 = new stockAccount(fileName);
+            Console.WriteLine(stock49.to_string());
+
+
+            while (z > 0)
+            {
+                Console.WriteLine("\nEnter a number to do which operation:");
+                Console.WriteLine("1)addAccount\t2)printReport\t3)valueOf\t4)buy\t5)sell\t6)EXIT");
+                int r = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (r)
+                {
+                    case 1:
+                        stock49.addAccount();
+                        break;
+                    case 2:
+                        stock49.printReport();
+
+                        break;
+
+                    case 3:
+                        stock49.valueOf();
+                        break;
+                    case 4:
+                        Console.WriteLine("enter the value of symbol to add the share to accout");
+                        string val = Console.ReadLine();
+                        Console.WriteLine("enter the value of amount to add the share to accout");
+                        int n = Convert.ToInt16(Console.ReadLine());
+                        stock49.buy(n, val);
+                        break;
+                    case 5:
+                        Console.WriteLine("enter the value of symbol to sell the share to accout");
+                        string vall = Console.ReadLine();
+                        Console.WriteLine("enter the value of amount to sell the share to accout");
+                        int n1 = Convert.ToInt16(Console.ReadLine());
+                        stock49.sell(n1, vall);
+                        break;
+                    case 6:
+                        z = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Oops a valid number to do which operation:");
+                        break;
+
+                }
+            }
         }
+
     }
 }
